148519e8032e30ede178b87ce5b59b75bc381bf9
Outsider <outsideris@gmail.com>
2018-02-14 05:57:51 +0900

    remove nmt model
    it is default now.

--- src/translate.js
+++ src/translate.js
@@ -50,7 +50,7 @@ const markdownTagSelector = () => {
 const translate = (text, API_KEY, LANGUAGE) => {
   const options = {
     method: 'POST',
-    body: `key=${API_KEY}&q=${encodeURIComponent(text)}&source=en&target=${LANGUAGE}&model=nmt&format=text`, // `format=text` keeps new line characters
+    body: `key=${API_KEY}&q=${encodeURIComponent(text)}&source=en&target=${LANGUAGE}&format=text`, // `format=text` keeps new line characters
     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8;' }
   };
 
